4777df04ac56ce10b27ef6c053959bbf8bbe71db
Matthew Sackman <matthew@rabbitmq.com>
2013-03-01 17:49:50 +0000

    Much better integration with the NodeJS REPL. Supports multi-line input, and resuming after a HALT. However, this has meant the Node/BVM REPL is now much better than the web one, so probably some more refactoring is needed.

--- src/repl.js
+++ src/repl.js
@@ -4,21 +4,59 @@
         'use strict';
 
         var repl = require('repl'),
-            e, i, bvmRepl;
+            parseError = {},
+            ev, processResult, bvmRepl, callback, undef;
 
-        e = function (cmd, context, filename, callback) {
-            cmd = cmd.substring(1, cmd.length - 2); // drop the surrounding '( and )';
-            try {
-                callback(null, JSON.stringify(bvmRepl.interpret(cmd)));
-            } catch (e) {
-                // Temp fix because syntax errors are getting lost /
-                // wrecked by the repl
-                callback(null, 'Error: ' + (e && (e.toString() || '' + e)));
+        // Note a lot of the code here is to work with the badly
+        // documented (and designed) NodeJS REPL module. You'll need
+        // to read a lot of its code to figure out some of the
+        // weirdness here.
+
+        ev = function (cmd, context, filename, cb) {
+            if (cmd.charAt(0) === '(' && cmd.charAt(cmd.length - 1) === ')') {
+                cmd = cmd.substring(1, cmd.length - 2); // drop the surrounding '( and )';
+            } else if ('err' in parseError) {
+                delete parseError.err;
+                // 2nd time here, and we errored the 1st time. Need to error again.
+                cb('SyntaxError');
+                return;
+            }
+
+            processResult(bvmRepl.interpret(cmd), cb);
+        };
+
+        processResult = function (result, cb) {
+            if (result === parseError) {
+                result = parseError.err;
+                bvmRepl.interpret.log(result.toString() || '' + result);
+                // strongarm the REPL modules notion of SyntaxError detection
+                cb('SyntaxError');
+            } else if (result === undef) {
+                // must have hit a HALT opcode. Save out callback for use later on.
+                callback = cb;
+                bvmRepl.interpret.log("BVM Halted. Use '.resume' to resume.");
+                cb(null);
+            } else {
+                try {
+                    cb(null, JSON.stringify(result));
+                } catch (errJSON) {
+                    // Almost certainly just a cyclical JSON error issue.
+                    bvmRepl.interpret.log('Result Display Error: ' +
+                                          (errJSON && (errJSON.toString() || '' + errJSON)));
+                    cb(null);
+                }
             }
         };
 
         bvmRepl = function () {
-            return repl.start({eval: e, prompt: 'bvm> ', useColors: false});
+            var r = repl.start({eval: ev, prompt: 'bvm> ', useColors: false, ignoreUndefined: true});
+            r.defineCommand('resume', {
+                help: 'Resume the BVM (for example after a HALT opcode)',
+                action: function () {
+                    processResult(bvmRepl.interpret.resume(), callback);
+                }
+            });
+            return r;
         };
 
         // Doing requires in here because of the change in the context
@@ -32,18 +70,30 @@
             bvmRepl.interpret = function (codeStr) {
                 assembly = nuAssembler();
                 assembly.source = codeStr;
-                assembly.parse().toJSON();
+                try {
+                    assembly.parse().toJSON();
+                } catch (err) {
+                    parseError.err = err;
+                    return parseError;
+                }
 
-                if (cpu) {
+                if (cpu !== undef) {
                     return cpu.resume(nuSegment(assembly.json));
                 } else {
                     cpu = nuCPU(nuSegment(assembly.json));
+                    bvmRepl.interpret.resume = function () {
+                        return cpu.resume();
+                    };
+                    bvmRepl.interpret.log = cpu.log;
                     return cpu.boot();
                 }
             };
 
             return bvmRepl.interpret(codeStrO);
         };
+
+        bvmRepl.interpret.resume = function () {};
+        bvmRepl.interpret.log = console.log.bind(console);
 
         return bvmRepl;
 
